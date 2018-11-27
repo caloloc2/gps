51b05352b4a2aefab6bf5445fbec6fdd649ff694
caloloc2 <calolomino@gmail.com>
2018-11-26 21:18:18 -0500

    Respuesta para app

---
+++ php/dato.php
@@ -0,0 +1,17 @@
+<?php
+
+$respuesta['estado'] = false;
+
+try{
+    require 'meta.php';
+
+    $dato = Meta::Consulta("SELECT * FROM registros ORDER BY id_registro DESC LIMIT 1");
+
+    if (count($dato)>0){
+        $respuesta['datos'] = $dato;
+    }
+}catch(Exception $e){
+    $respuesta['error'] => $e->getMessage();
+}
+
+echo json_encode($respuesta);
